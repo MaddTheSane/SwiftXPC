9df9162a10db052bbe39aa087354d726e24d8fa2
C.W. Betts <computers57@hotmail.com>
2015-02-18 14:39:50 -0700

    Add initial, lazy generator for the XPCDictionary.

--- SwiftXPC/Support/Generators.swift
+++ SwiftXPC/Support/Generators.swift
@@ -12,6 +12,7 @@ import XPC
 public struct XPCArrayGenerator: GeneratorType {
     private let theArray: XPCArray
     private var status: Int
+    
     private init(array: XPCArray) {
         status = 0
         if let copiedArray = array.copy() as? XPCArray {
@@ -45,14 +46,50 @@ extension XPCArray: ArrayLiteralConvertible {
     }
 }
 
-extension XPCDictionary: DictionaryLiteralConvertible {
+public struct XPCDictGenerator: GeneratorType {
+    private let keyArray: [String]
+    private let internalDict: [String: XPCObject]
+    private var i = 0
+    
+    public mutating func next() -> (key: String, object: XPCObject)? {
+        if i > keyArray.count {
+            let key = keyArray[i++]
+            let obj = internalDict[key]!
+            return (key, obj)
+        } else {
+            return nil
+        }
+    }
+    
+    init(dictionary: XPCDictionary) {
+        var preKeyArray = [String]()
+        var preDict = [String: XPCObject]()
+        //We're using xpc_dictionary_apply to iterate over the array
+        xpc_dictionary_apply(dictionary.objectPointer, { (name, object) -> Bool in
+            let swiftName = String(CString: name, encoding: NSUTF8StringEncoding)!
+            preKeyArray.append(swiftName)
+            preDict[swiftName] = nativeTypeForXPCObject(object)
+            
+            return true
+        })
+        keyArray = preKeyArray
+        internalDict = preDict
+    }
+}
+
+extension XPCDictionary: DictionaryLiteralConvertible, SequenceType {
     typealias Key = String
     typealias Value = XPCObject
+    typealias Generator = XPCDictGenerator
     
     public convenience init(dictionaryLiteral elements: (String, XPCObject)...) {
         self.init()
         for (key, value) in elements {
             self[key] = value
         }
+    }
+    
+    public func generate() -> XPCDictGenerator {
+        return XPCDictGenerator(dictionary: self)
     }
 }
