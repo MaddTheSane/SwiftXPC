bd7749680cf54a01240ac0b1ea934df3120c32e5
C.W. Betts <computers57@hotmail.com>
2015-11-16 21:34:32 -0700

    Minor documentation formatting.

--- SwiftXPC/Transport/Service.swift
+++ SwiftXPC/Transport/Service.swift
@@ -17,20 +17,20 @@ public final class XPCService {
         }
     }
     
-    /// beginTransaction() and endTransaction() can be used as a "keep-alive" mechanism
+    /// `beginTransaction()` and `endTransaction()` can be used as a "keep-alive" mechanism
     /// to avoid the runtime killing an XPC service while it is performing background work.
-    /// You must make sure that you remember to call endTransaction(), as otherwise the
-    /// XPC service will *never* die. Note that you can use performTransaction() to ensure that
-    /// endTransaction() is called automatically.
+    /// You must make sure that you remember to call `endTransaction()`, as otherwise the
+    /// XPC service will *never* die. Note that you can use `performTransaction()` to ensure that
+    /// `endTransaction()` is called automatically.
     public class func beginTransaction() {
         xpc_transaction_begin()
     }
     
-    /// beginTransaction() and endTransaction() can be used as a "keep-alive" mechanism
+    /// `beginTransaction()` and `endTransaction()` can be used as a "keep-alive" mechanism
     /// to avoid the runtime killing an XPC service while it is performing background work.
-    /// You must make sure that you remember to call endTransaction(), as otherwise the
-    /// XPC service will *never* die. Note that you can use performTransaction() to ensure that
-    /// endTransaction() is called automatically.
+    /// You must make sure that you remember to call `endTransaction()`, as otherwise the
+    /// XPC service will *never* die. Note that you can use `performTransaction()` to ensure that
+    /// `endTransaction()` is called automatically.
     public class func endTransaction() {
         xpc_transaction_end()
     }
