1a9f949d47222207f34766cb177c289c095985ec
C.W. Betts <computers57@hotmail.com>
2017-10-04 13:13:37 -0600

    Poke with the UUID class.

--- SwiftXPC/Objects/UUID.swift
+++ SwiftXPC/Objects/UUID.swift
@@ -12,37 +12,39 @@ import XPC
 private let UUIDLength: UInt = (128 / 8)
 
 public final class XPCUUID : XPCObject {
-    public convenience init(UUID: Foundation.UUID) {
-        var aNSUUIDBytes = [UInt8](repeating: 0, count: 16)
-        (UUID as NSUUID).getBytes(&aNSUUIDBytes)
+    public convenience init(uuid UUID: Foundation.UUID) {
+        var tmpUUIDByptes = UUID.uuid
+        let aNSUUIDBytes = withUnsafeBytes(of: &tmpUUIDByptes) { (someBytes) -> [UInt8] in
+            Array(UnsafeBufferPointer(start: someBytes.baseAddress?.bindMemory(to: UInt8.self, capacity: someBytes.count), count: someBytes.count))
+        }
         self.init(nativePointer: xpc_uuid_create(aNSUUIDBytes))
     }
     
-    public convenience init(UUID: CFUUID) {
-        let uuidStr = CFUUIDCreateString(kCFAllocatorDefault, UUID) as String
+    public convenience init(uuid: CFUUID) {
+        let uuidStr = CFUUIDCreateString(kCFAllocatorDefault, uuid) as String
         let ourUUID = Foundation.UUID(uuidString: uuidStr)!
-        self.init(UUID: ourUUID)
+        self.init(uuid: ourUUID)
     }
     
-    public convenience init?(UUIDString: String) {
-        if let ourUUID = Foundation.UUID(uuidString: UUIDString) {
-            self.init(UUID: ourUUID)
+    public convenience init?(uuidString: String) {
+        if let ourUUID = Foundation.UUID(uuidString: uuidString) {
+            self.init(uuid: ourUUID)
         } else {
             return nil
         }
     }
     
     public convenience init() {
-        self.init(UUID: Foundation.UUID())
+        self.init(uuid: Foundation.UUID())
     }
     
-    public var UUID: Foundation.UUID {
+    public var uuid: Foundation.UUID {
         let ourBytes = xpc_uuid_get_bytes(objectPointer)!
         return (Foundation.NSUUID(uuidBytes: ourBytes) as Foundation.UUID)
     }
     
     public var stringValue: String {
-        return UUID.uuidString
+        return uuid.uuidString
     }
     
     public class func generateUUID() -> XPCUUID {
@@ -51,5 +53,28 @@ public final class XPCUUID : XPCObject {
     
     override public var description: String {
         return "UUID: \(stringValue)"
+    }
+}
+
+// Deprecated
+extension XPCUUID {
+    @available(*, unavailable, renamed: "uuid")
+    public var UUID: Foundation.UUID {
+        fatalError()
+    }
+    
+    @available(*, unavailable, renamed: "init(uuid:)")
+    public convenience init(UUID: Foundation.UUID) {
+        fatalError()
+    }
+    
+    @available(*, unavailable, renamed: "init(uuid:)")
+    public convenience init(UUID: CFUUID) {
+        fatalError()
+    }
+    
+    @available(*, unavailable, renamed: "init(uuidString:)")
+    public convenience init?(UUIDString: String) {
+        fatalError()
     }
 }
